b8f07cce88e1506db55c888910961ad4a26588f1
sadasadsad <dfdfdfdsf>
2023-03-03 15:05:43 +0700

    complete 1

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc, char* argv[]){
+    int num = 0,arv,round = argc;        
+            if(argc == 1){
+            cout << "Please input numbers to find average."; 
+        }
+    else { 
+    for(int i = 1 ; i < round ; i++){
+        num += atoi(argv[i]);
+    }
+    arv = num/(round-1);
+    cout << "---------------------------------"<<endl;
+    cout << "Average of "<< round-1 << " numbers = "<<arv<<endl;
+    cout << "---------------------------------"<<endl;
+    }
+    return 0;
+}
